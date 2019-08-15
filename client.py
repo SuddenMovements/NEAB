d82387e32baad08a9416e54a23eb8dd50d608bb6
yacoubb <yacoub.ahmedy@gmail.com>
2019-08-15 12:26:24 +0100

    Client callbacks
    AgarioClient class now has a register_callback function which hooks into a number of socketio events as well as class-specific functions.

--- client.py
+++ client.py
@@ -25,7 +25,6 @@ class AgarioClient:
             if self.player_control:
                 cv2.setMouseCallback("agario" + self.index, self.mouse_callback)
         self.frame = np.full((self.screenHeight, self.screenWidth, 3), 255, np.uint8)
-        # self.target = {"theta": 0, "mag": 0}
         self.target = {"x": 0, "y": 0}
 
         # variables needed for rendering
@@ -41,52 +40,69 @@ class AgarioClient:
         self.virusStroke = list(map(lambda x: int(x * 255), hex2color("#19D119")))
         self.virusStrokeWeight = 4
 
+        self.callbacks = {}
+
+        self.frame_count = 0
+        self.last_frame_time = 0
+
         self.start()
 
     def start(self):
         self.register_socketio_callbacks()
         self.socket.connect("http://localhost:3000")
+        self.frame_count = 0
+        self.last_frame_time = time.time()
+
+        if "start" in self.callbacks:
+            self.callbacks["start"]()
+
+        fc = 0
+        fc_start = time.time()
         if self.player_control:
-            last_frame_time = time.time()
-            frame_count = 0
             while self.alive:
                 try:
                     self.render()
                     self.move()
-                    frame_count += 1
-                    if frame_count % 120 == 0:
-                        print(frame_count / (time.time() - last_frame_time))
+                    fc += 1
+                    if fc % 30 == 0:
+                        # print("render framerate", fc / (time.time() - fc_start))
+                        fc_start = time.time()
+                        fc = 0
                 except KeyboardInterrupt:
                     self.stop()
                     break
-            try:
-                self.socket.wait()
-            except KeyboardInterrupt:
-                self.stop()
 
     def stop(self):
         self.alive = False
         self.socket.disconnect()
         cv2.destroyAllWindows()
+        if "stop" in self.callbacks:
+            self.callbacks["stop"]()
 
     def register_socketio_callbacks(self):
         @self.socket.event
         def handshake():
-            print("got handshake")
+            # print("got handshake")
             playerInfo = {}
             if self.spectator:
                 playerInfo["type"] = "spectator"
             else:
                 playerInfo["type"] = "player"
             self.socket.emit("handshake", playerInfo)
+            if "handshake" in self.callbacks:
+                self.callbacks["handshake"]()
 
         @self.socket.event
         def playerInfo(info):
             self.playerCoords = {"x": info["x"], "y": info["y"]}
             self.playerMass = info["mass"]
+            self.update_screen_size_from_mass()
+            if "playerInfo" in self.callbacks:
+                self.callbacks["playerInfo"]()
 
         @self.socket.event
         def gameSetup(mapObject):
+            print(self.index, "got gamesetup")
             self.cells = mapObject["cells"]
             for f in mapObject["food"]:
                 self.food[f["id"]] = f
@@ -94,6 +110,8 @@ class AgarioClient:
                 self.masses[m["id"]] = m
             for v in mapObject["viruses"]:
                 self.viruses[v["id"]] = v
+            if "gameSetup" in self.callbacks:
+                self.callbacks["gameSetup"]()
 
         @self.socket.event
         def gameUpdate(info):
@@ -105,33 +123,56 @@ class AgarioClient:
             for f in info["addFood"].values():
                 self.food[f["id"]] = f
             for f in info["deleteFood"]:
-                del self.food[f]
+                # this check isnt a result of bad gamestate syncing
+                # but when clients join an already running server, the server may send info about food pellets that have been eaten
+                # before the client even had the food saved clientside
+                if f in self.food:
+                    del self.food[f]
 
             for m in info["updateMass"].values():
                 self.masses[m["id"]] = m
             for m in info["deleteMass"]:
-                del self.masses[m]
+                if m in self.masses:
+                    del self.masses[m]
 
             for v in info["updateVirus"].values():
                 self.viruses[v["id"]] = v
             for v in info["deleteVirus"]:
-                del self.viruses[v]
+                if v in self.viruses:
+                    del self.viruses[v]
+
+            self.frame_count += 1
+            if self.frame_count % 30 == 0:
+                # print("update framerate", self.frame_count / (time.time() - self.last_frame_time))
+                self.last_frame_time = time.time()
+                self.frame_count = 0
+            if "gameUpdate" in self.callbacks:
+                self.callbacks["gameUpdate"]()
 
         @self.socket.event
         def dead():
             print(self.index, "died")
-            self.stop()
+            if "dead" in self.callbacks:
+                self.callbacks["dead"]()
+            self.socket.emit("respawn")
+            # self.stop()
 
     def move(self):
         self.socket.emit("move", self.target)
+        if "move" in self.callbacks:
+            self.callbacks["move"]()
 
     def fire(self):
         # print("fire")
         self.socket.emit("fire")
+        if "fire" in self.callbacks:
+            self.callbacks["fire"]()
 
     def split(self):
         # print("split")
         self.socket.emit("split", False)
+        if "split" in self.callbacks:
+            self.callbacks["split"]()
 
     def mouse_callback(self, event, x, y, flags, param):
         if event == cv2.EVENT_MOUSEMOVE:
@@ -223,21 +264,34 @@ class AgarioClient:
                     self.fire()
                 if key == ord(" "):
                     self.split()
-
+        if "render" in self.callbacks:
+            self.callbacks["render"](frame)
         return frame
 
     def take_action(self, action):
-        # self.target["theta"] = action["theta"]
-        # self.target["mag"] = action["mag"]
-        self.target["x"] = action["mag"] * math.cos(action["theta"])
-        self.target["y"] = action["mag"] * math.sin(action["theta"])
+        self.target["x"] = action["x"]
+        self.target["y"] = action["y"]
+        # print(action)
+        # self.target["x"] = action["mag"] * math.cos(action["theta"])
+        # self.target["y"] = action["mag"] * math.sin(action["theta"])
         self.move()
         if action["fire"]:
             self.fire()
 
         if action["split"]:
             self.split()
+        if "render" in self.callbacks:
+            self.callbacks["take_action"]()
+
+    def register_callback(self, event_name, callback):
+        """Socket.io callbacks are: handshake, playerInfo, gameSetup, gameUpdate, dead.
+        Class callbacks are: start, stop, move, fire, split, render, take_action.
+        """
+        if not event_name in self.callbacks:
+            self.callbacks[event_name] = callback
+        else:
+            raise KeyError(event_name + " callback already registered!")
 
 
 if __name__ == "__main__":
-    AgarioClient(0, 600, True, True, False)
+    client = AgarioClient(16, 600, True, True, False)
